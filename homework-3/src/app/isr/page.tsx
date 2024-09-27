a3afdf4a3e17e24ec58c78c3961a2f7c051b5c68
Jo <johj703@nate.com>
2024-09-27 20:43:55 +0900

    fix: ISR 방식으로 페이지 렌더링 구현하기

--- homework-3/src/app/isr/page.tsx
+++ homework-3/src/app/isr/page.tsx
@@ -1,16 +1,23 @@
-export default async function ISRPage() {
+type Post = {
+  id: number;
+  title: string;
+  author: string;
+};
+
+async function getPosts(): Promise<Post[]> {
   const res = await fetch("http://localhost:4000/posts", {
-    next: {
-      revalidate: 5,
-    },
+    next: { revalidate: 60 },
   });
+  if (!res.ok) throw new Error("데이터를 불러오는 중 에러가 발생했습니다.");
+  return res.json();
+}
 
-  const data = await res.json();
-
+export default async function ISRPage() {
+  const posts = await getPosts();
   return (
     <div>
       ISRPage
-      <div>{JSON.stringify(data)}</div>
+      <pre>{JSON.stringify(posts)}</pre>
     </div>
   );
 }
