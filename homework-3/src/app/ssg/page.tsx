3f6423114083d996032e0ad8620d8954104500a4
Jo <johj703@nate.com>
2024-09-27 19:57:19 +0900

    fix: SSG 방식으로 페이지 렌더링 구현하기

--- homework-3/src/app/ssg/page.tsx
+++ homework-3/src/app/ssg/page.tsx
@@ -1,14 +1,21 @@
+type Post = {
+  id: number;
+  title: string;
+  author: string;
+};
+
+async function getPosts(): Promise<Post[]> {
+  const res = await fetch("http://localhost:4000/posts");
+  if (!res.ok) throw new Error("데이터를 불러오는 중 에러가 발생했습니다.");
+  return res.json();
+}
+
 export default async function SSGPage() {
-  const res = await fetch("http://localhost:4000/posts", {
-    cache: "force-cache",
-  });
-
-  const data = await res.json();
-
+  const posts = await getPosts();
   return (
     <div>
       SSGPage
-      <div>{JSON.stringify(data)}</div>
+      <pre>{JSON.stringify(posts)}</pre>
     </div>
   );
 }
