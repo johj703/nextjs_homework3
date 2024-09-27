a7610e8ea4a383debc6683f8f6763e1c77e22a26
Jo <johj703@nate.com>
2024-09-27 15:16:16 +0900

    fix: db.json에 있는 데이터를 불러오기 위한 useEffect 작성

--- homework-3/src/app/csr/page.tsx
+++ homework-3/src/app/csr/page.tsx
@@ -8,7 +8,8 @@ type Post = {
 };
 
 export default function CSRPage() {
-  const [state, setState] = useState("");
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // fetch("http://localhost:4000/posts")
@@ -16,7 +17,22 @@ export default function CSRPage() {
     //   .then((data) => {
     //     setState(JSON.stringify(data));
     //   });
-  });
+    fetch("http://localhost:4000/posts")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("데이터를 불러오는 중 에러가 발생했습니다.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("데이터 가져오는 것을 실패했습니다. :", error);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
